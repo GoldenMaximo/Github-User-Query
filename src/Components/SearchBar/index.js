169af03d6d8a921ea569a930dca79c79fd4298d6
gmaximo <gfmaximo97@gmail.com>
2019-10-14 09:22:49 -0300

    Change routing from Redirect component to WithNavigation

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -1,32 +1,62 @@
-import React from 'react';
-// import SearchIcon from './../../Assets/Icons/Search Icon.svg'
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+
+// vou usar useSelector pra pega o loading e fazer conditional rendering no icon do button
+import { useSelector, useDispatch } from 'react-redux';
+import { Creators as getUserData } from '../../Store/Ducks/getUserData';
+
 import { ReactComponent as SearchIcon } from '../../Assets/Icons/Search Icon.svg';
 import { ReactComponent as LoadingSpinner } from '../../Assets/Icons/Loading Spinner.svg';
 import './index.css';
 
-const SearchBar = props => (
-    <form className="form d-flex row align-items-center" onSubmit={props.onSubmit}>
-        <input
-            type="text"
-            name="searchInput"
-            id="searchInput"
-            className="Search-Input"
-            style={{
-                width: props.inputWidth,
-                minWidth: props.inputMinWidth,
-                maxWidth: props.inputMaxWidth,
-            }}
-            onChange={props.onChangeText}
-            value={props.value}
-        />
-        <button type="submit" className="d-flex justify-content-center">
-            {props.loading ? (
-                <LoadingSpinner />
-            ) : (
-                <SearchIcon />
-            )}
-        </button>
-    </form>
-);
+const SearchBar = props => {
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState('');
 
-export default SearchBar;
+    // const error = useSelector(state => state.getUserData.error);
+    const loading = useSelector(state => state.getUserData.loading);
+    const userData = useSelector(state => state.getUserData.userData);
+
+    useEffect(() => {
+        if (Object.entries(userData).length) {
+            props.history.push(`/${userData.login}`);
+        }
+    }, [userData]);
+
+    const submitHandler = event => {
+        event.preventDefault();
+        dispatch(getUserData.get(username));
+    };
+
+    const onChangeTextHandler = event => {
+        setUsername(event.target.value);
+    };
+
+    return (
+        <form className="form d-flex row align-items-center" onSubmit={submitHandler}>
+            <input
+                type="text"
+                name="searchInput"
+                id="searchInput"
+                className="Search-Input"
+                style={{
+                    width: props.inputWidth,
+                    minWidth: props.inputMinWidth,
+                    maxWidth: props.inputMaxWidth,
+                }}
+                onChange={onChangeTextHandler}
+                value={username}
+            />
+            <button type="submit" className="d-flex justify-content-center">
+                {loading ? (
+                    <LoadingSpinner />
+                ) : (
+                    <SearchIcon />
+                )}
+            </button>
+        </form>
+    );
+};
+
+export default withRouter(SearchBar);
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,51 +1,17 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-
-// vou usar useSelector pra pega o loading e fazer conditional rendering no icon do button
-import { useSelector, useDispatch } from 'react-redux';
-import { Creators as getUserData } from '../../Store/Ducks/getUserData';
-
+import React from 'react';
 
 import SearchHeading from '../../Components/SearchHeading';
 import SearchBar from '../../Components/SearchBar';
 import './index.css';
 
 
-const Home = () => {
-    const dispatch = useDispatch();
-    const [username, setUsername] = useState('');
-
-    const error = useSelector(state => state.getUserData.error);
-    const loading = useSelector(state => state.getUserData.loading);
-    const userData = useSelector(state => state.getUserData.userData);
-
-    if (Object.entries(userData).length || error) {
-        return (
-            <Redirect to={`/${userData.login}`} />
-        );
-    }
-
-    const submitHandler = event => {
-        event.preventDefault();
-        dispatch(getUserData.get(username));
-    };
-
-    const onChangeTextHandler = event => {
-        setUsername(event.target.value);
-    };
-
-    return (
-        <div className="home d-flex justify-content-center align-items-center">
-            <SearchHeading />
-            <SearchBar
-                value={username}
-                onChangeText={onChangeTextHandler}
-                onSubmit={submitHandler}
-                loading={loading}
-                inputWidth="32.6vw"
-            />
-        </div>
-    );
-};
+const Home = () => (
+    <div className="home d-flex justify-content-center align-items-center">
+        <SearchHeading />
+        <SearchBar
+            inputWidth="32.6vw"
+        />
+    </div>
+);
 
 export default Home;
